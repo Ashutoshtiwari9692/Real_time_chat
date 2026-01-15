e9b71de96405f47781c3b9235e137c40d5e77c81
Ashutoshtiwari9692 <ashutosh29704@gmail.com>
2026-01-15 17:32:35 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: baseURL: "https://real-time-chat-backend-ashutosh.onrender.com"
-,
+  baseURL: "https://real-time-chat-backend-ashutosh.onrender.com",
 });
 
 export function setAuthToken(token) {
-  if (token) api.defaults.headers.common.Authorization = `Bearer ${token}`;
-  else delete api.defaults.headers.common.Authorization;
+  if (token) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common["Authorization"];
+  }
 }
+
